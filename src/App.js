94af8fbd40c1b5456516f72620c286a395db02dc
JackelineGois <jackelinedemoraisgois@gmail.com>
2024-03-18 12:08:19 +0000

    Implemented and Tested Bootswatch/Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div>hello world!</div>;
+import React from "react";
+
+import "bootswatch/dist/flatly/bootstrap.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
